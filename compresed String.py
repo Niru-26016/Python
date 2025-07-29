336e7e87a30de9cb0499acaa4ad648c46bfe5c9c
Niranjan Reddy <niru26016@gmail.com>
2025-07-29 19:57:37 +0530

    added to repo

--- compresed String.py
+++ compresed String.py
@@ -8,9 +8,9 @@ for i in range(len(s)-1):
     if s[i]==s[i+1]:
         count+=1
     else:
-        l_aplha.append(s[i])
+        l_alpha.append(s[i])
         l_num.append(count)
         count=1
 for i in range (len(l_num)):
-    s2+=l_aplha[i]+str(l_num[i])
+    s2+=l_alpha[i]+str(l_num[i])
 print(s2)
